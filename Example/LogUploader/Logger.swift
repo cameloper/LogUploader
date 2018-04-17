3be4c4e5811436cca6c37e063564e6b2b0b74392
Ihsan B. Yilmaz <cameloper@cameloper.com>
2018-04-17 16:24:44 +0200

    Set demo app
    Configured the demo app to test the log uploader

---
+++ Example/LogUploader/Logger.swift
@@ -0,0 +1,38 @@
+//
+//  Logger.swift
+//  LogUploader_Example
+//
+//  Created by Yilmaz, Ihsan on 16.04.18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import XCGLogger
+import LogUploader
+
+let log: XCGLogger = {
+    let log = XCGLogger.default
+    
+    log.setup(level: .debug, showFunctionName: false, showLevel: true, showFileNames: true, showLineNumbers: true, showDate: true)
+    let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+    let logFileURL = URL(fileURLWithPath: documentsPath + "/xcglog.json")
+    
+    let requestUrl = URL(string: "http://localhost:8080/")
+    
+    let conf = LogUploaderConfiguration(requestURL: requestUrl!)
+    let jsonDestination = JSONDestination(owner: log, fileURL: logFileURL, identifier: "logger.jsonLogger", uploaderConf: conf)
+    
+    jsonDestination.showDate = true
+    jsonDestination.showLevel = true
+    jsonDestination.showThreadName = true
+    jsonDestination.showLogIdentifier = false
+    
+    jsonDestination.logQueue = XCGLogger.logQueue
+    log.add(destination: jsonDestination)
+    log.logAppDetails()
+    
+    log.logAppDetails()
+    
+    log.dateFormatter?.dateFormat = "HH:mm:ss.SSS"
+    return log
+}()
--- Example/LogUploader/ViewController.swift
+++ Example/LogUploader/ViewController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtLogs: UITextView!
+    @IBOutlet weak var txtNewLog: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtLogs.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func newLog(_ sender: Any) {
+        guard let text = txtNewLog.text else {
+            return
+        }
+        
+        logMsg(text: text)
+    }
+    
+    func logMsg(text: String) {
+        log.debug(txtNewLog.text)
+        txtLogs.text = "\(txtLogs.text ?? "")\n>\(text)"
+    }
+    
+    @IBAction func uploadLogs(_ sender: Any) {
+        log.uploadLogs(from: "logger.jsonLogger") { result in
+            switch result {
+            case .success:
+                self.logMsg(text: "Log upload successful")
+            case .failure(let error):
+                self.logMsg(text: "Log upload failed. \(error)")
+            }
+        }
+    }
+    
 }
 
