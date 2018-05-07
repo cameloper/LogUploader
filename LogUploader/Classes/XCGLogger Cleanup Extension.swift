33d92366210ea0f4e3c7e5977cad41aa836de6cc
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2018-05-07 15:32:38 +0200

    [F, #4] Added extension

---
+++ LogUploader/Classes/XCGLogger Cleanup Extension.swift
@@ -0,0 +1,23 @@
+//
+//  XCGLogger Cleanup Extension.swift
+//  LogUploader
+//
+//  Created by Yilmaz, Ihsan on 07.05.18.
+//
+
+import Foundation
+import XCGLogger
+
+/// Extension of XCGLogger where the cleanup methods for log files stand
+extension XCGLogger {
+    
+    /// Delete all log files that are strored
+    /// - Returns: Boolean: Is cleanup successful
+    public func deleteAllLogFiles() -> Bool {
+        var result = true
+        
+        
+        
+        return result
+    }
+}
