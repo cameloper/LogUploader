4f930c79a26f2d32803e73810a8a3147c6ca7fe9
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2018-05-07 17:25:52 +0200

    [F, #4] Add method deleteSuccessfulLogFiles()

--- LogUploader/Classes/XCGLogger Cleanup Extension.swift
+++ LogUploader/Classes/XCGLogger Cleanup Extension.swift
@@ -26,10 +26,38 @@ extension XCGLogger {
             return true
         }
         
-        let fileManager = FileManager()
+        // Delete contents and return result
+        return deleteContents(of: uploaderFolders)
+    }
+    
+    /// Deletes log files of all successful uploads
+    /// - Returns: Boolean: Cleanup is successful
+    public func deleteSuccessfulLogFiles() -> Bool {
+        // Get all Custom File Destinations
+        let destinations = self.destinations.compactMap { $0 as? CustomFileDestination }
+        let uploaderFolders = Set<URL>(destinations.compactMap {
+            $0.uploaderConfiguration?.uploader.homeURL
+        })
         
-        // For all uploader folders...
-        for folderURL in uploaderFolders {
+        // If empty, return false
+        guard !uploaderFolders.isEmpty else {
+            self.warning("There are no uploaders with existing logfiles!")
+            return true
+        }
+        
+        // Get folders for successful uploads
+        let successfulFolders = uploaderFolders.map { $0.appendingPathComponent("successful", isDirectory: true) }
+        
+        // Delete contents and return result
+        return deleteContents(of: Set(successfulFolders))
+        
+    }
+    
+    /// Deletes contents of given folders
+    func deleteContents(of folders: Set<URL>) -> Bool {
+        let fileManager = FileManager()
+        // For all folders...
+        for folderURL in folders {
             do {
                 // ...get contents...
                 let contents = try fileManager.contentsOfDirectory(atPath: folderURL.path)
@@ -40,7 +68,6 @@ extension XCGLogger {
                 return false
             }
         }
-        
         return true
     }
 }
